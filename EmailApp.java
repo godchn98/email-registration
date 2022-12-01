f361285d68b8d2892818cb4b3fad7fd2ee6377f4
YJ Park <100175696+godchn98@users.noreply.github.com>
2022-12-01 14:50:55 +0700

    scan name function added

--- EmailApp.java
+++ EmailApp.java
@@ -1,10 +1,22 @@
 package emailApp;
 
+import java.util.Scanner;
+
 public class EmailApp {
 
 	public static void main(String[] args) {
 		
-		Email em1 = new Email("John", "Smith");
+		System.out.println("Welcome to Email registration\n");
+		
+		System.out.println("Enter first name");
+		Scanner scanFirstname = new Scanner(System.in);
+		String firstname = scanFirstname.nextLine();
+		System.out.println("Enter last name");
+		Scanner scanLastName = new Scanner(System.in);
+		String lastname = scanLastName.nextLine();
+
+
+		Email em1 = new Email(firstname, lastname);
 		
 		System.out.println(em1.showInfo());
 		
